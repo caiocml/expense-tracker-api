453c3b6ab8571ca04f8cc2044e7bdc835e6e201d
Caio Cesar <caiocmelolopes@gmail.com>
2021-06-22 11:21:49 -0300

    testando commit

--- expense-tracker-api/src/main/java/br/com/caiocesar/expense/tracker/api/ExpenseTrackerApiApplication.java
+++ expense-tracker-api/src/main/java/br/com/caiocesar/expense/tracker/api/ExpenseTrackerApiApplication.java
@@ -25,6 +25,7 @@ public class ExpenseTrackerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
+		System.out.println("Aplication Started");
 	}
 	
 	@Autowired
